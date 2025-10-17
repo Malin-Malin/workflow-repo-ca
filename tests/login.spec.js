adfdfb99ccc251e7761f8c247c2c52374ee8b36f
Malin Skrettingland <malinersdal@hotmail.com>
2025-10-17 11:46:15 +0200

    feat: add initial tests for login and navigation functionality

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,11 +1,22 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 test.describe("Login Page", () => {
-  test("login form", async ({ page }) => {
+  test("Login form", async ({ page }) => {
     await page.goto("/login");
     await page.fill('input[name="email"]', process.env.TEST_USERNAME);
     await page.fill('input[name="password"]', process.env.TEST_PASSWORD);
     await page.click('button[type="submit"]');
-    await expect(page).toHaveURL("/");
+    await page.waitForURL("/");
+    // Check if we see logout button - means we're logged in
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+  test("Invalid credentials", async ({ page }) => {
+    await page.goto("/login");
+    await page.fill('input[name="email"]', process.env.TEST_USERNAME);
+    await page.fill('input[name="password"]', "wrongpassword");
+    await page.click('button[type="submit"]');
+    await expect(page.getByRole("alert")).toHaveText(
+      "Invalid email or password",
+    );
   });
 });
